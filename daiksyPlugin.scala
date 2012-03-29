51d99755efa83bef798851b330680a01875f7db5
Daisuke Kasuya <ksydisk780921@gmail.com>
2012-03-29 22:44:09 +0900

    first commit

---
+++ daiksyPlugin.scala
@@ -0,0 +1,31 @@
+package plugins
+
+import play.api._
+import twitter4j.Twitter
+import twitter4j.TwitterFactory
+
+/** 誰得プラグイン */
+class daiksyPlugin(app: Application) extends Plugin {
+  val name = "daiksy Plugin"
+  private lazy val twitter = new TwitterFactory().getInstance
+    
+  /** とりあえず常にtrueにしとこ. */
+  override lazy val enabled = {
+    true
+  }
+
+  /** ログでも出すか. */
+  override def onStart() {
+    Logger.trace("変態が...はじまる！！")
+  }
+
+  /** ログ出しとく. */
+  override def onStop() {
+    Logger.trace("タイ━━━━｜｜Φ｜（｜゜｜∀｜゜｜）｜Φ｜｜━━━━ホ！！")
+  }
+  
+  /** daiksyのタイムラインを取得する誰得関数 */
+  def timeline = {
+    twitter.getUserTimeline("daiksy")
+  }
+}
