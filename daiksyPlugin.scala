89e99a5b542556bc5dd4ce3a06ca353c91dcba85
Daisuke Kasuya <ksydisk780921@gmail.com>
2012-03-30 00:14:29 +0900

    関数追加

--- daiksyPlugin.scala
+++ daiksyPlugin.scala
@@ -1,14 +1,16 @@
 package plugins
 
 import play.api._
+import scala.collection.JavaConversions
 import twitter4j.Twitter
 import twitter4j.TwitterFactory
+import twitter4j.Status
 
 /** 誰得プラグイン */
 class daiksyPlugin(app: Application) extends Plugin {
   val name = "daiksy Plugin"
   private lazy val twitter = new TwitterFactory().getInstance
-    
+
   /** とりあえず常にtrueにしとこ. */
   override lazy val enabled = true
 
@@ -17,7 +19,13 @@ class daiksyPlugin(app: Application) extends Plugin {
 
   /** ログ出しとく. */
   override def onStop() = Logger.trace("タイ━━━━｜｜Φ｜（｜゜｜∀｜゜｜）｜Φ｜｜━━━━ホ！！")
-  
+
   /** daiksyのタイムラインを取得する誰得関数 */
   def timeline = twitter.getUserTimeline("daiksy")
+
+  /**
+   *  daiksyの最新ツイートを取得. 本当は取得できなかったときの処理とかいるけどとりあず無視.
+   *  getUserTimelineからのresuponseListをscalaのコレクションとして扱って高階関数とか使いたいなー.  
+   */
+  def lastTweet = twitter.getUserTimeline("daiksy").get(0).getText
 }
